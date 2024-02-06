8457e18775fd7c5a50c04fef983aa1e9b771d236
Khushbu <kjindal1221@gmail.com>
2024-02-06 19:02:26 +0530

    update

--- client/src/pages/BookingPage.js
+++ client/src/pages/BookingPage.js
@@ -152,7 +152,7 @@ const BookingPage = () => {
 
       <p>Source: {source}</p>
       <p>Destination: {destination}</p>
-      <p>Provider ID: {providerId}</p>
+      <p>Price: {price}</p>
       <div className="container">
         {provider !== undefined ? (
           <div>
